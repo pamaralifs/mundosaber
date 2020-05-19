1bd97c2c27230597a350ad3923f868725b9382a0
Paulo Amaral <paulodoamaralcosta@hotmail.com>
2020-05-18 23:01:13 -0300

    Alteração do settings.py para produção

--- mundodosaber/settings.py
+++ mundodosaber/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%52(!fxt^yfa_3$3*xhfy&*oliuedi2!*wukk+n$6^e75hp(@$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','*','cemundosaber.pythoanywhere.com'] #
-
+#ALLOWED_HOSTS = ['localhost','127.0.0.1','*','cemundosaber.pythoanywhere.com'] #
+ALLOWED_HOSTS = ['localhost','127.0.0.1','cemundosaber.com.br'] #
 
 # Application definition
 
